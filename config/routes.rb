fdc7deae63e7521f815a09300d30c46e65cd0c09
Rick G <lacunainc04@gmail.com>
2017-03-04 23:14:26 -0800

    Make hotels index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :room_categories
   resources :hotels
+  root 'hotels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
